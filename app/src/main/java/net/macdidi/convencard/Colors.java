076f1b7f95d4b8f638c721cf1f96b199f490332c
xinwei <candy5410tw@gmail.com>
2018-11-07 19:44:59 +0800

    還原主頁顯示照片之前

--- app/src/main/java/net/macdidi/convencard/Colors.java
+++ app/src/main/java/net/macdidi/convencard/Colors.java
@@ -6,7 +6,7 @@ import android.graphics.Color;
 public enum Colors {
 
     LIGHTGREY("#D3D3D3"), BLUE("#33B5E5"), PURPLE("#AA66CC"),
-    GREEN("#99CC00"), ORANGE("#FFBB33"), RED("#FF4444"),WHITE("#ffffff");
+    GREEN("#99CC00"), ORANGE("#FFBB33"), RED("#FF4444");
 
     private String code;
 
